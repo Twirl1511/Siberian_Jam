0ae91558c72f5b3cad3260a472a30d82e44d133d
Mike L <Twirl1511@yandex.ru>
2021-04-03 18:52:09 +0300

    !F песок разлетается от падения объекта

---
+++ Assets/Scripts/SandParticleSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandParticleSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject _sandPartSystem;
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Block block))
+        {
+            GameObject sandParticles;
+            Vector3 position = new Vector3(block.transform.position.x, -10f, 0);
+            sandParticles = Instantiate(_sandPartSystem, position, Quaternion.Euler(-90,0,0));
+            Destroy(sandParticles, 1);
+        }
+    }
+}
