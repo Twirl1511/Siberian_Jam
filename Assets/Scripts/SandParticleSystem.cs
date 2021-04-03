86a0f37842747840e0784d869a1a94544c235f7f
Mike L <Twirl1511@yandex.ru>
2021-04-03 19:08:21 +0300

    !F fix

--- Assets/Scripts/SandParticleSystem.cs
+++ Assets/Scripts/SandParticleSystem.cs
@@ -15,7 +15,7 @@ public class SandParticleSystem : MonoBehaviour
         if (collision.gameObject.TryGetComponent(out Block block))
         {
             GameObject sandParticles;
-            Vector3 position = new Vector3(block.transform.position.x, -10f, 0);
+            Vector3 position = new Vector3(collision.collider.ClosestPointOnBounds(block.transform.position).x, -10f, 0);
             sandParticles = Instantiate(_sandPartSystem, position, Quaternion.Euler(-90,0,0));
             Destroy(sandParticles, 1);
         }
