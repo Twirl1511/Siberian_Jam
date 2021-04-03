7471305d4be263a79ab464a3060aee5d793e9f2a
Mike L <Twirl1511@yandex.ru>
2021-04-03 20:24:52 +0300

    !F bag fix

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,8 +6,30 @@ public class Block : MonoBehaviour
 {
     public bool IsActive = true;
     [HideInInspector] public Rigidbody Rigi;
+    [HideInInspector] public float RayCastLength;
+    private RaycastHit _raycastHit;
+    [SerializeField] private LayerMask _layerMask;
 
     private void Start() {
         Rigi = GetComponent<Rigidbody>();
+        InvokeRepeating(nameof(RayCastToFloor), 1, 1);
+    }
+
+    public void RayCastToFloor()
+    {
+        Physics.Raycast(transform.position, Vector3.down, out _raycastHit, Mathf.Infinity, _layerMask);
+        if(_raycastHit.distance > RayCastLength)
+        {
+            RayCastLength = _raycastHit.distance;
+        }
+        print(RayCastLength);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out SandParticleSystem sand))
+        {
+            RayCastLength = 0;
+        }
     }
 }
--- Assets/Scripts/SandParticleSystem.cs
+++ Assets/Scripts/SandParticleSystem.cs
@@ -10,13 +10,19 @@ public class SandParticleSystem : MonoBehaviour
         
     }
 
+    [System.Obsolete]
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent(out Block block))
         {
             GameObject sandParticles;
             Vector3 position = new Vector3(collision.contacts[0].point.x, -10f, 0);
+            //if(block.RayCastLength <= 7)
+            //{
+
+            //}
             sandParticles = Instantiate(_sandPartSystem, position, Quaternion.Euler(-90,0,0));
+            
             Destroy(sandParticles, 1);
         }
     }
